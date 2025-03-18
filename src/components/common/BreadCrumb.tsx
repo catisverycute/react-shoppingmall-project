c22da61209dfff61ba53ec11029f86d6b2af5dec
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-03-19 03:57:49 +0900

    breadcrumb 생성

---
+++ src/components/common/BreadCrumb.tsx
@@ -0,0 +1,24 @@
+import { MENUS } from "../../constants/constants";
+
+interface BreadCrumbProps {
+  category?: string;
+  crumb?: string;
+}
+
+export default function BreadCrumb({ category, crumb }: BreadCrumbProps) {
+  const categoryFilter = category
+    ? MENUS[category.toUpperCase() as keyof typeof MENUS] || category
+    : "";
+  const crumbFilter = crumb
+    ? MENUS[crumb.toUpperCase() as keyof typeof MENUS] || crumb
+    : "";
+
+  return (
+    <div className="flex justify-start text-sm breadcrumbs">
+      <ul>
+        <li>{categoryFilter}</li>
+        <li>{crumbFilter}</li>
+      </ul>
+    </div>
+  );
+}
