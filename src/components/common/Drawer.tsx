60fc59d3fa29e2a1cd5e88e7d87bf8a823328223
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-03-19 03:57:31 +0900

    검색 리스트 생성

---
+++ src/components/common/Drawer.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import useProducts from "../../store/useProducts";
+import { useNavigate } from "react-router-dom";
+
+interface SearchProps {
+  inputValue?: string;
+}
+
+export default function Drawer({ inputValue }: SearchProps) {
+  const { products } = useProducts();
+  const [filteredProducts, setFilteredProducts] = useState(products);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (inputValue) {
+      const filterProduct = products.filter((p) =>
+        p.title.toLocaleLowerCase().includes(inputValue.toLocaleLowerCase())
+      );
+      setFilteredProducts(filterProduct);
+    } else {
+      setFilteredProducts([]);
+    }
+  }, [inputValue, products]);
+
+  if (!inputValue || filteredProducts.length === 0) {
+    return null;
+  }
+
+  const clickBtn = (id: number) => {
+    navigate(`/product/${id}`);
+    setFilteredProducts([]);
+  };
+
+  return (
+    <div className="absolute">
+      <ul className="!fixed  left-auto sm:absolute sm:top-14 menu flex-nowrap w-full dropdown-content sm:w-64 max-h-96 shadow text-base-content overflow-y-auto overflow-x-hidden bg-white dark:bg-gray-600">
+        <li>
+          {filteredProducts.map((p) => (
+            <button
+              type="button"
+              className="text-left js-searchedItem"
+              onClick={() => clickBtn(p.id)}
+            >
+              <span className="text-gray-600 dark:text-white line-clamp-2 ">
+                {p.title}
+              </span>
+            </button>
+          ))}
+        </li>
+      </ul>
+    </div>
+  );
+}
