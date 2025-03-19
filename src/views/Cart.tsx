d453a650187c831878712c00cc1ffaa6d01cd0bb
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-03-20 04:13:17 +0900

    css 수정

--- src/views/Cart.tsx
+++ src/views/Cart.tsx
@@ -3,7 +3,7 @@ import CartView from "../components/carts/CartView";
 
 const Cart = (): JSX.Element => {
   return (
-    <section className="pt-4 lg:pt-5 pb-4 lg:pb-8 px-4 xl:px-2 xl:container mx-auto">
+    <section className="pt-6 lg:pt-12 pb-6 lg:pb-12 px-4 xl:px-2 xl:container mx-auto">
       <CartView />
     </section>
   );
